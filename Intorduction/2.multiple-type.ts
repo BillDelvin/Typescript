cf16c35b7ec7e94bf18866e3a0b15b8369361660
Bill Delvin <43694045+BillDelvin@users.noreply.github.com>
2019-07-17 16:17:42 +0700

    typescript

---
+++ Intorduction/2.multiple-type.ts
@@ -0,0 +1,4 @@
+let example1 : boolean | number = 35;
+let example2 : number = 35;
+let exmaple3 : string = "Hello World";
+
---
+++ Intorduction/3.Implicit-and-Explicit-typing.ts
@@ -0,0 +1,13 @@
+const arrayExample : number[] = [1,2,3,4];
+
+// explicit
+let example1 : boolean | number = 35;
+
+let example2 = arrayExample.reduce( (num1, num2) => num1 + num2 )
+
+// implicit
+let example3 = 'Hello world';
+
+let example4: undefined = undefined;
+
+let example5: null = null;
