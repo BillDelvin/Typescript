92133c6c520a9a7770346f9bf63529df464b3e2d
Bill <billdelvin6@gmail.com>
2019-07-21 09:02:07 +0700

    MEAN

---
+++ MEAN/AngularApp/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatCardModule, MatButtonModule} from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
