b88b43f1bc6d423a9f3da3f6d6efa6edd38eaefe
Bill <billdelvin6@gmail.com>
2019-08-01 19:43:13 +0700

    tour of heroes

--- heroes-app/src/app/app.module.ts
+++ heroes-app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MassagesComponent } from './massages/massages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { MassagesComponent } from './massages/massages.component';
     HeroesComponent,
     HeroDetailComponent,
     MassagesComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
--- heroes-app/src/app/hero.service.ts
+++ heroes-app/src/app/hero.service.ts
@@ -8,11 +8,14 @@ import {MassageService} from './massage.service';
   providedIn: 'root'
 })
 export class HeroService {
+  getHero(id: number) {
+    throw new Error("Method not implemented.");
+  }
   constructor(private massageService : MassageService) {
 
   }
 
-  getHeroes(): Observable<Hero[]> {
+  getHeroes(id: number ): Observable<Hero[]> {
     // TODO: sent the massage_after_fetching the heroes 
     this.massageService.add('HeroService : fetched Heroes')
     return of(HEROES);
