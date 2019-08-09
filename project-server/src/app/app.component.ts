1d5629fcb87f003f8afd991fa520b3b4759b3bbb
Bill <billdelvin6@gmail.com>
2019-08-09 10:04:55 +0700

    udemy-project

--- project-server/src/app/app.component.ts
+++ project-server/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project-server';
+  serverElements = [{type:'server', name:'testserver', content:'just a test!'}];
 }
