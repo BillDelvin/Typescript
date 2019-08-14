3839f4bd5b0a37481ffbd5d33dd059d83922e375
Bill <billdelvin6@gmail.com>
2019-08-14 20:16:45 +0700

    about-directive

--- about-directive/src/app/app.module.ts
+++ about-directive/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
-import { UnlessDirective } from './unless.directive';
+import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
