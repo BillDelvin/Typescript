29e1ffd140bed807b228cbe0e802959b4ed5b41f
Bill <billdelvin6@gmail.com>
2019-08-15 15:25:35 +0700

    service&DI

--- service/src/app/account/account.component.ts
+++ service/src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  @Input() account: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSetTo(){
+    
   }
 
 }
--- service/src/app/logging-service/logging.service.ts
+++ service/src/app/logging-service/logging.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class LoggingService {
 
   constructor() { }
+
+  logStatusChange(status:string){
+    console.log('A server status changed, new status: ' + status);
+  }
 }
