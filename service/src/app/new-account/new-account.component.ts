004fccdc5e3048733a1629a62118f0ba3bc80056
Bill <billdelvin6@gmail.com>
2019-08-16 09:15:52 +0700

    service&DI

--- service/src/app/new-account/new-account.component.ts
+++ service/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging-service/logging.service';
 
 @Component({
   selector: 'app-new-account',
@@ -7,6 +8,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService){
+
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
