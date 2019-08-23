0af2fccee59f02e9f2e9d7a75dc2b173b46e0686
Bill <billdelvin6@gmail.com>
2019-08-23 16:49:36 +0700

    course-router

--- router-course/src/app/servers/server/server.component.ts
+++ router-course/src/app/servers/server/server.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-import { ServersService } from '../servers.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { ServersService } from '../servers.service';
 
 @Component({
   selector: 'app-server',
@@ -14,11 +13,11 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe(
       (params: Params) => {
-        this.server = this.serversService.getServer(params['id']);
+        this.server = this.serversService.getServer(+params['id']);
       }
     );
   }
